3163ddab1231f7275d2838dc2df2eb0a82b692df
Oleg Plaksin <oplak@yandex.ru>
2022-09-27 22:03:18 +0300

    init commit

---
+++ src/main/java/com/example/medfiz/MedfizApplication.java
@@ -0,0 +1,13 @@
+package com.example.medfiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedfizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedfizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medfiz/MedfizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medfiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedfizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
